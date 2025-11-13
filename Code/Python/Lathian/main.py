3c11171b3851bc33d40c414e5bdd337d63255775
glaukos20 <glaukos20@gmail.com>
2025-11-12 20:31:52 -0600

    why

---
+++ Code/Python/Lathian/main.py
@@ -0,0 +1,37 @@
+
+def bio()
+	while pronouns_Selected:
+		player_Pronouns = input ("What would you like to be refered to by?\n1. Masculin pronouns(he/him)\n2. feminine pronouns(she/her)\n3. nuetral pronouns(they/them)\n4. custom")
+		if player_Pronouns == 1:
+			player_Pronoun_Subjective = "he"
+			player_Pronoun_Objective = "him"
+			player_Pronoun_Possessive = "his"
+			player_Pronoun_Reflexive = "himself"
+			pronouns_Selected = True
+		elif player_Pronouns == 2:
+			player_Pronoun_Subjective = "she"
+			player_Pronoun_Objective = "her"
+			player_Pronoun_Possessive = "hers"
+			player_Pronoun_Reflexive = "herself"
+			pronouns_Selected = True
+			
+		elif player_Pronouns == 3:
+			player_Pronoun_Subjective = "they"
+			player_Pronoun_Objective = "them"
+			player_Pronoun_Possessive = "theirs"
+			player_Pronoun_Reflexive = "themself"
+			pronouns_Selected = True
+			
+		elif player_Pronouns == 4:
+			player_Pronouns = input ( "1. \n2. \n3. \n4. \n")
+		else :
+			print ("Please input the number of your selection")
+	
+	bio = (player_Pronoun_Subjective, player_Pronoun_objective, player_Pronoun_Possessive, player_Pronoun_Reflexive,)
+print ("Weclome to Lathian")
+player_Name = input("What's your name?")
+print ("Ok, ", player_Name, ".")
+y/n_temp = print ("Do you mind if i ask you a few questions?\nY/N")
+if y/n_temp == y:
+	bio()
+
