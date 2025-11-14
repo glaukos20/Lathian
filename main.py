5bea74d2d5f881869b7ad364f6f8c480b0116984
glaukos20 <glaukos20@gmail.com>
2025-11-13 18:59:22 -0600

    why

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import player_Info
 print ("Weclome to Lathian")
-player_Name = input("What's your name?")
+player_Name = input("What's your name?\n")
 print ("Ok, ", player_Name, ".")
-y/n_temp = print ("Do you mind if I ask you a few questions?")
-y/n_temp[0].lower()
-if y/n_temp[0].lower() == y:
-	pronouns = player_Info.pronouns()
+yn_temp = input ("Do you mind if I ask you a few questions?")
+
+yn_temp = yn_temp[0].lower()
+if yn_temp == "y":
+	pronouns = player_Info.get_pronouns()
+elif yn_temp == "n":
+	yn_temp = input ("You wish to skip charcheter custamization \nConfirm y/n") 
+	yn_temp = yn_temp[0].lower()
+	if yn_temp == "n":
+		pronouns = player_Info.get_pronouns()
+	elif yn_temp == "y":
+		print("ok")
