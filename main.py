e6f40a605e64003331f18cbd8f31ead140787d46
glaukos20 <glaukos20@gmail.com>
2025-11-13 21:56:02 -0600

    input verification

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import player_Info
+import invalid_Input
+print ("\033[4;36m hello world \033[0m")
 print ("Weclome to Lathian")
 player_Name = input("What's your name?\n")
 print ("Ok, ", player_Name, ".")
-yn_temp = input ("Do you mind if I ask you a few questions?")
-
-yn_temp = yn_temp[0].lower()
-if yn_temp == "y":
-	pronouns = player_Info.get_pronouns()
-elif yn_temp == "n":
-	yn_temp = input ("You wish to skip charcheter custamization \nConfirm y/n") 
+while True:
+	yn_temp = input ("Do you mind if I ask you a few questions?")
 	yn_temp = yn_temp[0].lower()
-	if yn_temp == "n":
+	if yn_temp == "y":
 		pronouns = player_Info.get_pronouns()
-	elif yn_temp == "y":
-		print("ok")
+		break
+	elif yn_temp == "n":
+		while True:
+			yn_temp = input ("You wish to skip charcheter custamization \nConfirm y/n") 
+			yn_temp = yn_temp[0].lower()
+			if yn_temp == "n":
+				pronouns = player_Info.get_pronouns()
+				break
+			elif yn_temp == "y":
+				break
+			else :
+				invalid_Input
+	else :
+		invalid_Input
---
+++ move.py
@@ -0,0 +1,6 @@
+import invalid_Input
+def input_Verify(Input):
+    Input = Input.lower()
+    if Input in ["left", "right", "up", "down", "north", "east", "south", "west", "straight", "forward", "backward"]:
+        move
+    if Input in ["look", "look around", "watch", "inspect", "look closer","open", "close", "run", "hide","kill", "breath", "crawl", ]:
